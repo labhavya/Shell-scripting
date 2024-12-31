322839b23ece72e573a9a35c42a017a022138f6e
Bhavya <bhavya571@gmail.com>
2024-12-31 17:41:46 +0530

    adding numbers

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -2,7 +2,7 @@
 
 NUMBER1=$1
 NUMBER2=$2
-TIMESTAMP=$(date)   # PUT YOUR COMMAND INSIDE BRACES.
+TIMESTAMP=$(date)   # PUT YOUR COMMAND INSIDE BRACES and preceed with $ to assign to a variable.
 echo "script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 echo "Sum of 2 numbers $NUMBER1, $NUMBER2 is : $SUM"
---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES= ("pushpa", "rrr", "devara")
+#index starts from 0, size is 3
+
+echo "First movie is : ${MOVIES[0]}"
+echo "secondrst movie is : ${MOVIES[1]}"
+echo "third movie is : ${MOVIES[2]}"
+echo "all movies are: ${MOVIES[@]}"
