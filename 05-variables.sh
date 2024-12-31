92797e74b17b4d80f4c5f895d9d88a123eb2ebf7
Bhavya <bhavya571@gmail.com>
2024-12-31 11:30:39 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,5 +3,5 @@
 echo "Please enter your username::"
 read -s USERNAME                #here username is variablename
 echo "user name entered :: $USERNAME"
-each "please enter your password"
+echo "please enter your password:"
 read -s PASSWORD
