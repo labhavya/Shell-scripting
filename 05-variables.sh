908ea2f8100625a9d695150224c6a230cb541d00
Bhavya <bhavya571@gmail.com>
2024-12-31 17:30:03 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#I want to enter the values manully.
 echo "Please enter your username::"
 read -s USERNAME                #here username is variablename
 echo "user name entered :: $USERNAME"
---
+++ 06-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+TIMESTAMP=$(DATA)   # PUT YOUR COMMAND INSIDE BRACES.
+echo "script executed at: $TIMestamp"
+SUM=$(($NUMBER1+$NUMBER2))
+echo "Sum of 2 numbers is : $SUM"
