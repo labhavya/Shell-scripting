3d14151bd04a83b2c7a1b6943929ed68f933e92b
Bhavya <bhavya571@gmail.com>
2024-12-31 18:19:12 +0530

    adding numbers

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 NUMBER1=$1
-if [ $NUMBER1 -gt 100]
+if [ $NUMBER1 -gt 100 ]
 # -gt,-lt, -eq, -ge, -le
 then
-echo "given number $NUMBER1 is greater than 100"
+    echo "given number $NUMBER1 is greater than 100"
 else
-echo "given number $NUMBER1 is less than 100
+    echo "given number $NUMBER1 is less than 100"
 fi
