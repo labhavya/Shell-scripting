4334a8215c7da9b81f85c58b8434520e6b711165
Bhavya <bhavya571@gmail.com>
2025-01-01 12:38:02 +0530

    user id print

--- 11-myversion.sh
+++ 11-myversion.sh
@@ -4,11 +4,10 @@ if [ USER -ne 0 ]
 then 
     echo "ERROR:: user doens't have permission to install"
     exit 1
-
 dnf install mysql -y
-
 if [ $? -ne 0 ]
-then 
-    echo "ERROR:: mysql installation failed..............."
+then
+    echo " ERROR:: mysql installation failed...................."
 else
-    echo "mysql installed successfully"
+    echo "mysql installation successful......"
+fi
