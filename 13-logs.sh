409559ce62ba2b33550952e4089bf1a76bb0ae9f
Bhavya <bhavya571@gmail.com>
2025-01-02 15:47:43 +0530

    added git commands

---
+++ 13-logs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USER -ne 0 ]
+then 
+    echo "ERROR:: user doens't have permission to install"
+    exit 1
+fi 
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2....$G success"
+        
+    else 
+        echo -e "$2....$R failed"
+        exit 1
+    fi
+}
+dnf list installed | grep mysql-server
+if [ $? -ne 0 ]
+then
+    echo "going to install mysql.........."
+    dnf install mysql-server -y &>>$LOG_FILE_NAME
+    VALIDATE $? "MYSQL INSTALL IS"
+    
+else
+    echo -e "mysql is already ... $Y installed"
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+     echo "installing git"
+     dnf install git -y &>>$LOG_FILE_NAME
+     VALIDATE $? "GIT INSTALL IS"
+else
+    echo -e "git is already... $Y installed"
+fi
