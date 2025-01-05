7a07e6233164e7e13feb7a3b71d40d7e71f08872
Bhavya <bhavya571@gmail.com>
2025-01-05 13:19:03 +0530

    frontend server creation

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+#set colours
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#setup log folder:
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+mkdir -p /var/log/shellscript-logs
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+CHECK_ROOT()
+{
+   USERID=$(id -u)
+   if [ $USERID -ne 0 ]
+    then
+        echo "user doesn't have root access"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+if [ $1 -eq 0 ]
+   then
+   echo -e "$2......$G success $N"
+else
+   echo -e "$2.......$R failed $N"
+   exit 1
+fi
+}
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Fiels to be deleted is:: $FILES_TO_DELETE" 
+
