7c14760a409a0f94f626b1c3ea74353885e73a0d
Bhavya <bhavya571@gmail.com>
2025-01-07 12:57:45 +0530

    backup update

--- 17-backup.sh
+++ 17-backup.sh
@@ -3,10 +3,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 |cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-#LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+#LOG_FILE=$(echo $0 |cut -d "." -f1)
 LOG_FILE=$(echo $0 | aws -F  "/" '${print $NF} '| cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 mkdir -p /home/ec2-user/shellscript-logs
 
 SOURCEDIR=$1
