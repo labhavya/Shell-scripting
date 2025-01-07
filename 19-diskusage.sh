67e8ce30d9377d251df7ae38e7a80494511aa724
Bhavya <bhavya571@gmail.com>
2025-01-07 18:24:37 +0530

    backup update

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -2,7 +2,6 @@
 DISKUSAGE=$(df -hT |grep xfs)
 while read -r line
 do
-  echo $line
   USAGE=$(echo $line | awk -F " " '{print $6F}')
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
   echo "partiiton:$PARTITION; Usage:$USAGE"
