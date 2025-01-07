713edf0a21a21aecb9c042648f00bb4b9130f35f
Bhavya <bhavya571@gmail.com>
2025-01-07 18:23:16 +0530

    backup update

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -5,4 +5,5 @@ do
   echo $line
   USAGE=$(echo $line | awk -F " " '{print $6F}')
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
+  echo "partiiton:$PARTITION; Usage:$USAGE"
 done <<<$DISKUSAGE
