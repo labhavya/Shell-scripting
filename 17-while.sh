97f9f7d77bba876e91f36853877fa44315f3a871
Bhavya <bhavya571@gmail.com>
2025-01-08 16:56:27 +0530

    dish usage

--- 17-while.sh
+++ 17-while.sh
@@ -3,3 +3,11 @@ while read -r line
 do
 echo "$line"
 done < 16-delete-old-logs.sh
+
+echo "entering into second while loop"
+
+FILE =16-delete-old-logs.sh
+while read -r line
+do
+echo "$line"
+done < "$FILE"
