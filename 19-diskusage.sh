8082046c25185ef91729cd088b307789b1d8fe32
Bhavya <bhavya571@gmail.com>
2025-01-08 13:11:31 +0530

    dish usage

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 DISKUSAGE=$(df -hT |grep xfs)
 DISK_THRESHOLD=5 # real project, we monitor for 70
+MSG=""
 while read -r line
 do
   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)  # -F here is filed separator.
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
-  if [ $USAGE -gt $DISK_THRESHOLD ] 
+  if [ $USAGE -ge $DISK_THRESHOLD ] 
    then 
+      MSG+="High disk usage on partition: $PARTITION; Usage is: $USAGE
       echo "partiiton:$PARTITION; Usage:$USAGE"
   fi
   done <<<$DISKUSAGE
