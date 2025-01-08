f01f62db44813e1c4fc2b9b7e7b5e15c8bb7601b
Bhavya <bhavya571@gmail.com>
2025-01-08 16:41:48 +0530

    dish usage

--- 20-script1.sh
+++ 20-script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MSG="SCRIPT 1"
 GREET="Hi from SCRIPT-1"
-source ./21-script2.sh #places the other file script here
+source ./21-script2.sh #places the other file script here. second script executes in the process of script 1, so we can access the vaiables.
 echo "Hello from: $MSG"
 
 # sh 21-script2.sh  # in this case vairalbes can't be accessed form one script to other script.
---
+++ while.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+while read -r line
+do
+echo "$line"
+done < 16-delete-old-logs.sh
