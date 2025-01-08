fbc614519157d0df04ea83ba7c49d0b45f724f8d
Bhavya <bhavya571@gmail.com>
2025-01-08 11:17:46 +0530

    dish usage

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -3,7 +3,7 @@ DISKUSAGE=$(df -hT |grep xfs)
 DISK_THRESHOLD=5 # real project, we monitor for 70
 while read -r line
 do
-  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" f1)  # -F here is filed separator.
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)  # -F here is filed separator.
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
   
   echo "partiiton:$PARTITION; Usage:$USAGE"
