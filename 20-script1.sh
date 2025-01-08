6a6c4d89f051f9abd53ad058222db93bc29dc71f
Bhavya <bhavya571@gmail.com>
2025-01-08 15:36:51 +0530

    dish usage

--- 20-script1.sh
+++ 20-script1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 MSG="SCRIPT 1"
+GREET="Hi from SCRIPT-1"
+source 21-script2.sh
 echo "Hello from: $MSG"
-sh 21-script2.sh
+
+# sh 21-script2.sh  # in this case vairalbes can't be accessed form one script to other script.
